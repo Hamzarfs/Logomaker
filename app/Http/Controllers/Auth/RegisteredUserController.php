9f26dbb2ce4d43eb6e2c3ab8c255e6315931cac8
suraqa <m.suraqa@gmail.com>
2024-08-09 22:01:01 +0500

    Updated phone number validation

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,7 +33,7 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
-            'phone' => ['required', 'regex:/^(\+44\s?7\d{3}|\+44\s?\d{2}\s?\d{4}|\+44\s?\d{3}\s?\d{3}|\+44\s?\d{4}\s?\d{2}|\+44\s?\d{5})\s?\d{3}\s?\d{3}$/'],
+            'phone' => ['required', 'regex:/^(\+44\s?(\d{2}|\d{3}|\d{4})\s?\d{3,4}\s?\d{3,4})$/'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ], [
             'phone.regex' => 'Phone number must be a valid UK number (+44 20 7123 4567)'
