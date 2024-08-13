b6cdb85f9129922f06b4f45b882c74d95fb85aa3
suraqa <m.suraqa@gmail.com>
2024-08-13 23:40:35 +0500

    Contact us & custom logo email template done

--- resources/views/mail/custom-logo.blade.php
+++ resources/views/mail/custom-logo.blade.php
@@ -1,9 +1,13 @@
-<div>
-    <h1>New Custom logo consultation Lead</h1>
-    <br>
-    <p>Name: {{ $data['name'] }}</p>
-    <p>Email: {{ $data['email'] }}</p>
-    <p>Phone: {{ $data['phone'] }}</p>
-    <p>Country: {{ $data['country'] }}</p>
-    <p>Message: {{ $data['message'] }}</p>
-</div>
+@extends('mail.layout')
+
+@section('content')
+    <div>
+        <h1>New Custom logo consultation Lead</h1>
+        <br>
+        <p>Name: {{ $data['name'] }}</p>
+        <p>Email: {{ $data['email'] }}</p>
+        <p>Phone: {{ $data['phone'] }}</p>
+        <p>Country: {{ $data['country'] }}</p>
+        <p>Message: {{ $data['message'] }}</p>
+    </div>
+@endsection
