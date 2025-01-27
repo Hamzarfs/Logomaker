9df8c2ed94c009439831083cd5df50be1c1abd2b
Suraqa Jafrani <m.suraqa@gmail.com>
2025-01-27 20:53:15 +0500

    Update blog.blade.php

--- resources/views/site/blog.blade.php
+++ resources/views/site/blog.blade.php
@@ -11,14 +11,14 @@
     <section class="blog-page py-5">
         <div class="container">
             <div class="blog-container">
-                <img src="{{ $blog->image_url }}" alt="{{ $blog->title }}" class="img-fluid">
+                <img src="{{ $blog->image_url }}" alt="{{ $blog->title }}" class="img-fluid d-block mx-auto">
 
                 <h1 class="text-center mt-4">{{ $blog->title }}</h1>
                 <div class="row g-2 align-items-center justify-content-between mb-3">
                     <div class="col-md-auto">
                         <div class="d-flex gap-3 align-items-center">
                             <h6 class="gray-font fw-bold m-0">Category:</h6>
-                            <div class="badge rounded-pill text-bg-success px-3 py-2">{{ $blog->category->name }}</div>
+                            <div class="badge rounded-pill text-bg-primary px-3 py-2">{{ $blog->category->name }}</div>
                         </div>
                     </div>
                     <div class="col-md-auto">
@@ -27,7 +27,7 @@
                             <div>
                                 <div class="d-flex flex-wrap gap-2 align-items-center ">
                                     @foreach ($blog->tags as $tag)
-                                        <div class="badge rounded-pill text-bg-success px-3 py-2">{{ $tag->name }}</div>
+                                        <div class="badge rounded-pill text-bg-primary px-3 py-2">{{ $tag->name }}</div>
                                     @endforeach
                                 </div>
                             </div>
